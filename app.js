93eed5977a7c86075ea312df386b78a41ee51289
Joseph-K-B <303josephb@gmail.com>
2021-06-28 10:57:35 -0700

    pictures in function working

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 // import functions
 // import { evenOdd } from './is-even';
-import { oddEven } from './is-even.js';
+// import { oddEven } from './is-even.js';
 import { evenOdd } from './is-even.js';
+// import { evenOdd2 } from './is-even.js';
 //import { thumbsUp} from './is-even.js';
 // import { isOddEven } from "./is-even.js";
 const input = document.getElementById('even-odd');
 const output = document.getElementById('answer');
 const submitBtn = document.getElementById('input-btn');
 const hidden = document.getElementsByClassName('hidden');
-const hidden2 = document.getElementsByClassName('hidden2');
+// const hidden2 = document.getElementsByClassName('hidden2');
 // reference needed DOM elements
 
 submitBtn.addEventListener('click', () =>{
-    console.log(typeof(input.value));
+    // console.log(typeof(input.value));
     const even = Number(input.value);
     const answer1 = evenOdd(even);
-    console.log(output);
-    output.textContent = answer1;
+    // console.log(output);
+    // output.textContent = answer1;
+
+});
 //     if (even) {
 //         return output.getElementsByClassName = hidden};
 //     else {
-//       return output.getElementsByClassName = hidden2};
-});
+//        return (output.getElementsByClassName = hidden2)};
 
-submitBtn.addEventListener('click', () => {
-    hidden[0].style.display = 'block';
-});
+
+// submitBtn.addEventListener('click', () => {
+//     hidden[0].style.display = 'block';
+// });
  
 
-submitBtn.addEventListener('click', () =>{
-    hidden2[0].style.display = 'block';
-});
+// submitBtn.addEventListener('click', () => {
+//     hidden2[0].style.display = 'block';
+// });
 
 // submitBtn.addEventListener('click', () => {
 //     thumbsUp[0].style.display = 'block';
--- is-even.js
+++ is-even.js
@@ -7,21 +7,33 @@
 //         document.getElementById('answer').innerHTML = num + 'is odd';
 //     }
 // }
+// const output = document.getElementById('answer');
+const hidden = document.getElementsByClassName('hidden');
+const output = document.getElementById('answer');
+// const hidden2 = document.getElementsByClassName('hidden2');
 
 export function evenOdd(num1) {
     if (num1 % 2 === 0){
-        return ('Even!');
+        return (output.textContent = 'Even!', (hidden[0].style.display = 'block'));
     }
-    else {
-        return ('Odd!');
+    else if (num1 % 2 !== 0){
+        return (output.textContent = 'Odd!', (hidden[1].style.display = 'block'));
     }
 }
+// export function evenOdd2(num1) {
+//     if (num1 % 2 === 0){
+//         return (hidden);
+//     }
+//     else {
+//         return (hidden2);
+//     }
+// }
 
-export function oddEven(num1){
-    if (num1 % 2 !== 0){
-        return ('Odd!');
-    }
-}
+// export function oddEven(num1){
+//     if (num1 % 2 !== 0){
+//         return ('Odd!');
+//     }
+// }
 
 // export function thumbsUp(num1) {
 //     if (num1 % 2 === 0){
