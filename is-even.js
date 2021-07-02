b5fd2008c72d5123bcef81d9bdc8f2cb9b625517
Justin W Soto <soto@pdx.edu>
2021-07-02 16:13:57 -0700

    it lives

--- is-even.js
+++ is-even.js
@@ -19,12 +19,12 @@ export function evenOdd(num1) {
     }
     if (num1 % 2 === 0){
         return (output.textContent = 'Even!', (hidden[0].style.display = 'block'),
-        (hidden[1].style.display = 'hidden'));
+        (hidden[1].style.display = 'none'));
     }
     // if (num1 % 2 !== 0){
     else 
         return (output.textContent = 'Odd!', (hidden[1].style.display = 'block'),
-        (hidden[0].style.display = 'hidden')
+        (hidden[0].style.display = 'none')
         );
 }
 
